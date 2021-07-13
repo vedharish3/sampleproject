1517c21f41df34f3e7deb3cb4f24195b7995458a
Harish Ved <ved.harish3@gmail.com>
2021-07-13 19:28:37 +0530

    Add AWS creds

--- src/sample/simple.py
+++ src/sample/simple.py
@@ -2,4 +2,6 @@ def add_one(number):
     number = number + 1
     `wrong python trun asdb asd backticks`
     justrandomword
+    aws_access_key = "AKIAIOSFODNN7EXAMPLE"
+    aws_secret+key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
     return number + 1
